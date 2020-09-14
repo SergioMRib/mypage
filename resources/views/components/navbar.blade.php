7c1f8922b7d85b55ab6ab381efcaec8137e7ba97
SergioMRib <sergio.m.rib@outlook.com>
2020-09-14 18:38:53 +0100

    Corrected social icons on topbar
    No longer a nav
    Using css grid

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,37 +1,27 @@
-<nav class="navbar is-fixed-top is-transparent" role="navigation" aria-label="main navigation">
+{{-- styles in custom.css --}}
 
-    <div id="navbar" class="navbar-menu is-active">
+<div class="social-icons is-transparent">
+    <div class="grid">
+        <p class="is-size-3 natural-font">Contact me</p>
+        <a class="social-icon" href="{{route('contact-me')}}">
+            <span class="icon">
+                <i class="fas fa-at fa-2x"></i>
+            </span>
+        </a>
 
-        <div class="navbar-end">
-            <p class="is-size-3 natural-font">Contact me</p>
-            @if ($current !== 'index')
-                <a class="navbar-item" href="/">
-                    <span class="icon">
-                        <i class="fas fa-home fa-2x"></i>
-                    </span>
-                </a>
-            @endif
+        <a class="social-icon" href="https://github.com/SergioMRib">
+            <span class="icon">
+                <i class="fab fa-github fa-2x"></i>
+            </span>
+        </a>
 
-            <a class="navbar-item" href="{{route('contact-me')}}">
-                <span class="icon">
-                    <i class="fas fa-at fa-2x"></i>
-                </span>
-            </a>
-
-            <a class="navbar-item" href="https://github.com/SergioMRib">
-                <span class="icon">
-                    <i class="fab fa-github fa-2x"></i>
-                </span>
-            </a>
-
-            <a class="navbar-item" href="https://www.linkedin.com/in/sergribeiro/">
-                <span class="icon">
-                    <i class="fab fa-linkedin fa-2x"></i>
-                </span>
-            </a>
-        </div>
+        <a class="social-icon" href="https://www.linkedin.com/in/sergribeiro/">
+            <span class="icon">
+                <i class="fab fa-linkedin fa-2x"></i>
+            </span>
+        </a>
     </div>
-</nav>
+</div>
 
 
 
