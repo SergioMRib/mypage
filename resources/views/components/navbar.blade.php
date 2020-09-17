26eff4e8cd47538c44f62e8053c76a8ca1aff3f9
SergioMRib <sergio.m.rib@outlook.com>
2020-09-17 19:01:08 +0100

    Changes to the social links layout

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -2,20 +2,21 @@
 
 <div class="social-icons is-transparent">
     <div class="grid">
-        <p class="is-size-3 natural-font">Contact me</p>
+        <p class="float-appeal is-size-3 natural-font">Contact me</p>
+
         <a class="social-icon" href="{{route('contact-me')}}">
             <span class="icon">
                 <i class="fas fa-at fa-2x"></i>
             </span>
         </a>
 
-        <a class="social-icon" href="https://github.com/SergioMRib">
+        <a href="https://github.com/SergioMRib">
             <span class="icon">
                 <i class="fab fa-github fa-2x"></i>
             </span>
         </a>
 
-        <a class="social-icon" href="https://www.linkedin.com/in/sergribeiro/">
+        <a href="https://www.linkedin.com/in/sergribeiro/">
             <span class="icon">
                 <i class="fab fa-linkedin fa-2x"></i>
             </span>
