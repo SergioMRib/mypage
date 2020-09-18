db1f74a803f6a6185f9cb0d8ecc3c12eba127c97
SergioMRib <sergio.m.rib@outlook.com>
2020-09-18 18:37:33 +0100

    changed content to test deployment 4

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
 
-    <p>This is the content deployed to staging</p>
+    <p>This is the content deployed to staging before production</p>
     {{-- @component('components.full-page-section')
         @component('components.card')
             @slot('title')
