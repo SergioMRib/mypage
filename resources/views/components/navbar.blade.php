88a2321211134468a38c8c978372aab9661c745c
SergioMRib <sergio.m.rib@outlook.com>
2020-10-21 23:06:51 +0100

    Social Links open in new tab

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -4,19 +4,19 @@
     <div class="grid">
         <p class="float-appeal is-size-3 natural-font">Contact me</p>
 
-        <a class="social-icon" href="{{route('contact-me')}}">
+        <a class="social-icon" href="{{route('contact-me')}}" target="_blank" rel="noopener noreferrer">
             <span class="icon">
                 <i class="fas fa-at fa-2x"></i>
             </span>
         </a>
 
-        <a href="https://github.com/SergioMRib">
+        <a href="https://github.com/SergioMRib" target="_blank" rel="noopener noreferrer">
             <span class="icon">
                 <i class="fab fa-github fa-2x"></i>
             </span>
         </a>
 
-        <a href="https://www.linkedin.com/in/sergribeiro/">
+        <a href="https://www.linkedin.com/in/sergribeiro/" target="_blank" rel="noopener noreferrer">
             <span class="icon">
                 <i class="fab fa-linkedin fa-2x"></i>
             </span>
