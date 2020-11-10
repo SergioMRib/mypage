6276622dd9da99825caf6fbb2beb3e8cf810d165
SergioMRib <sergio.m.rib@outlook.com>
2020-11-10 10:42:35 +0000

    Project's views ahs edit and show pages
    The respective controller methods were introduced in two earlier commits

--- resources/views/dashboard/projects/edit.blade.php
+++ resources/views/dashboard/projects/edit.blade.php
@@ -6,17 +6,63 @@
     <h3 class="is-title is-size-3">{{$current ?? ''}}</h3>
     <h3>This is the edit page</h3>
 
-    {{-- <form action="{{route('tags.store')}}" method="post">
+    <form action="{{route('projects.update', $project->id)}}" method="post" enctype="multipart/form-data">
         @csrf
+        @method('PUT')
         <div class="field">
-            <label for="name">Add tag</label>
+            <label for="name">Add title</label>
+            <input type="text" class="input"
+                name="name" id="tagName"
+                value="{{$project->name}}"
+                >
+        </div>
+        <div class="field">
+            <label for="description">Add description</label>
+            <textarea type="text" class="input"
+                name="description" id="description"
+                style="white-space: pre-line; white-space: pre-wrap; "
+                >{{$project->description}}</textarea>
+        </div>
+        <div class="field">
+            {{-- <label for="categories">Add categories</label> --}}
+            @foreach ($categories as $category)
+                <label for="{{$category->id}}">{{$category->name}}</label>
+                <input type="checkbox" id="{{$category->id}}" name="categories[]" value="{{$category->id}}">
+                |
+            @endforeach
+        </div>
+        <div class="field">
+            {{-- <label for="tags">Add tags</label> --}}
+            {{-- @foreach ($tags as $tag)
+                <label for="{{$tag->id}}">{{$tag->name}}</label>
+                <input type="checkbox" id="{{$tag->id}}" name="tags[]" value="{{$tag->id}}">
+                |
+            @endforeach --}}
+        </div>
+        <div class="field">
+            <label for="image">Add image</label>
+                <input type="file" class="input"
+                    name="image" id="image"
+                    value="{{$project->image}}"
+                    >
+        </div>
+        <div class="field">
+            <label for="">Add link</label>
                 <input type="text" class="input"
-                    name="name" id="tagName"
-                    placeholder="Name"
+                    name="link" id="link"
+                    value="{{$project->link}}"
+                    >
+        </div>
+        <div class="field">
+            <label for="">Add Github link</label>
+                <input type="text" class="input"
+                    name="githublink" id="githublink"
+                    value="{{$project->githublink}}"
                     >
         </div>
         <div class="form-group">
-            <button type="submit" class="button is-primary">Register</button>
+            <button type="submit" class="button is-primary">Save</button>
+            <a href="{{route('projects.index')}}" class="button is-warning">Cancel</a>
         </div>
         @if ($errors->any())
             <div class="is-danger">
@@ -27,29 +73,7 @@
                 </ul>
             </div>
         @endif
-    </form> --}}
-    {{-- @if (count($tags) > 0)
-        @foreach ($tags as $tag)
-            <div class="card">
-                <div class="card-header">{{$tag->name}}</div>
-                <div class="card-content">
-                    <p>Nome: {{$tag->name}}</p>
-                    <p>
-                        <form action="{{route('tags.destroy', $tag->id)}}" method="post">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="button">
-                                <i class="fas fa-trash"></i>
-                            </button>
-                        </form>
-
-                    </p>
-                </div>
-            </div>
-        @endforeach
-    @else
-        <p>Não existem tags</p>
-    @endif --}}
+    </form>
 
 
 
--- resources/views/dashboard/projects/show.blade.php
+++ resources/views/dashboard/projects/show.blade.php
@@ -4,6 +4,40 @@
 @section('content')
 
     <h3 class="is-title is-size-3">{{$current ?? ''}}</h3>
-    <h3>This is the show page</h3>
+    <div class="card">
+        <div class="card-header">{{$project->name}} || id: {{$project->id}}</div>
+        <div class="card-content">
+            <p>Nome: {{$project->name}}</p>
+            <p>Descrição:</p>
+            <p>
+                {!! nl2br(e($project->description ?? 'No description')) !!}
+            </p>
+            <p>
+                <img src="/storage/{{$project->image}}" alt="{{$project->name}} image">
+                Related categories:
+                @foreach ($project->categories as $cat)
+                    <p>
+                        {{$cat->name}}
+                    </p>
+                @endforeach
+            </p>
+            @if (isset($project->link) || isset($project->githublink))
+                <p>
+                    <a href="{{$project->link ?? ''}}">{{$project->link ?? ''}}</a> |
+                    <a href="{{$project->githublink ?? ''}}">{{$project->githublink ?? ''}}</a>
+                </p>
+            @endif
+
+            <p>
+                <form action="{{route('projects.destroy', $project->id)}}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="button">
+                        <i class="fas fa-trash"></i>
+                    </button>
+                </form>
+            </p>
+        </div>
+    </div>
 
 @endsection
