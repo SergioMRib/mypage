5668a9d797d4cefa1a4182a31730da45eab67b89
SergioMRib <sergio.m.rib@outlook.com>
2020-12-04 11:51:28 +0000

    bug fix - pages showed side bar even logged out

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,6 +1,25 @@
-@extends('layouts.dashboard')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>Sérgio's homepage</title>
 
-@section('content')
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+
+    </head>
+    <body class="bg-img">
+
+        <main>
+ 
+
+{{-- @extends('layouts.dashboard') --}}
+
+{{-- @section('content') --}}
     @component('components.full-page-section')
         @component('components.card')
             @slot('title')
@@ -59,4 +78,8 @@
 
         @endcomponent
     @endcomponent
-@endsection
+    </main>
+
+    <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
+</body>
+</html>
