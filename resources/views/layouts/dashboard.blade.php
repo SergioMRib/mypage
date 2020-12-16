6b5e00d5cce5b1a32415069141a0d4fe162524ff
SergioMRib <sergio.m.rib@outlook.com>
2020-12-16 17:30:45 +0000

    Changed title of page for dashboard on head

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Sérgio's dashboard</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
